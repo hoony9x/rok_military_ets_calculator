72b0abb1193c839ba94c2e7ac99656152c8c9180
Kihoon Han <khhan1993@gmail.com>
2018-07-28 13:40:52 +0900

    구조 잡기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import {
+  CssBaseline,
+  Grid,
+  Paper,
+  Hidden,
+  Typography
+} from '@material-ui/core';
+
+const styles = (theme) => ({
+  root: {
+    flexGrow: 1,
+    padding: '25px'
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }
+});
 
 class App extends Component {
   render() {
+    const { classes } = this.props;
+
     return (
       <React.Fragment>
-        <div>
-
+        <CssBaseline />
+        <div className={classes.root}>
+          <Grid container spacing={24} alignItems="center">
+            <Hidden xsDown>
+              <Grid item md sm />
+            </Hidden>
+            <Grid item md={6} sm={8} xs>
+              <Paper className={classes.paper}>Content</Paper>
+            </Grid>
+            <Hidden xsDown>
+              <Grid item md sm />
+            </Hidden>
+          </Grid>
         </div>
       </React.Fragment>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
